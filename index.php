b366f8d45b69a9d6b98cc9ac6b4702e574b50eeb
kai <coelacanth2505@gmail.com>
2021-01-24 19:47:41 +0900

    no message

--- index.php
+++ index.php
@@ -21,13 +21,25 @@
     <h2>Practice</h2>
     <pre>
       <?php
+
+      const DB_HOST = 'mysql:dbname=mydb;host=127.0.0.1;
+            charset = utf8';
+      const DB_USER = 'root';
+      const DB_PASSWORD = 'root';
       try {
-        $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;port=3306;charset=utf8', 'root', 'root');
+        $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+        // echo '接続成功';
       } catch (PDOException $e) {
         echo 'DB接続エラー: ' . $e->getMessage();
       }
+      // try {
+      //   $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;port=3306;charset=utf8', 'root', 'root');
+      // } catch (PDOException $e) {
+      //   echo 'DB接続エラー: ' . $e->getMessage();
+      // }
 
-      $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name ="もも",price=210,keyword="缶詰,ピンク,甘い"');
+
+      $count = $pdo->exec('INSERT INTO my_items SET id=1, item_name ="もも",price=210,keyword="缶詰,ピンク,甘い"');
       echo $count . '件のデータを挿入しました';
       ?>
     </pre>
