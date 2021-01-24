28f7f0d4f18c80e3c923dabe012765c5c8338711
kai <coelacanth2505@gmail.com>
2021-01-24 21:15:14 +0900

    61

--- index.php
+++ index.php
@@ -22,25 +22,30 @@
     <pre>
       <?php
 
-      const DB_HOST = 'mysql:dbname=mydb;host=127.0.0.1;
-            charset = utf8';
-      const DB_USER = 'root';
-      const DB_PASSWORD = 'root';
-      try {
-        $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
-        // echo '接続成功';
-      } catch (PDOException $e) {
-        echo 'DB接続エラー: ' . $e->getMessage();
-      }
+      // const DB_HOST = 'mysql:dbname=mydb;host=127.0.0.1;
+      //       charset = utf8';
+      // const DB_USER = 'root';
+      // const DB_PASSWORD = 'root';
       // try {
-      //   $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;port=3306;charset=utf8', 'root', 'root');
+      //   $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+      //   // echo '接続成功';
       // } catch (PDOException $e) {
       //   echo 'DB接続エラー: ' . $e->getMessage();
       // }
+      try {
+        $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;port=3306;charset=utf8', 'root', 'root');
+      } catch (PDOException $e) {
+        echo 'DB接続エラー: ' . $e->getMessage();
+      }
 
 
-      $count = $pdo->exec('INSERT INTO my_items SET id=1, item_name ="もも",price=210,keyword="缶詰,ピンク,甘い"');
-      echo $count . '件のデータを挿入しました';
+      // $count = $db->exec('INSERT INTO my_items SET maker_id=1,item_name="もも",price=210,keyword="缶詰,ピンク,甘い"');
+      // echo $count . '件のデータを挿入しました';
+
+      $records = $db->query('SELECT * FROM my_items');
+      while ($record = $records->fetch()) {
+        print($record['item_name'] . "\n");
+      }
       ?>
     </pre>
   </main>
