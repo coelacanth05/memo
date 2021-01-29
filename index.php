50ed425a383015de1f62906017035feda2563c7e
kai <coelacanth2505@gmail.com>
2021-01-29 22:06:47 +0900

    66

--- index.php
+++ index.php
@@ -48,6 +48,8 @@
         <time><?php print($memo['created_at']); ?></time>
         <hr>
       <?php endwhile; ?>
+
+      <a href="index.php?page=<?php print($page + 1); ?>"><?php print($page + 1); ?>ページ目へ</a>
     </article>
   </main>
 </body>
